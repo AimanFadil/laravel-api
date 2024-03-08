f6cc22ca55add39c29b9cf793928086347b29cd1
AimanFadil <aimanbello2001@gmail.com>
2024-03-08 14:52:07 +0100

    lead controller, migration, model and route

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -4,8 +4,41 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
+
+use App\Models\Lead;
+use App\Mail\NewContact;
 
 class LeadController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'nome' => 'required|max:40',
+            'cognome' => 'required|max:40',
+            'cell' => 'required|max:25',
+            'email' => 'required|max:150',
+            'meassaggio' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $new_lead = new Lead();
+        $new_lead->fill($data);
+        $new_lead->save();
+
+        Mail::to('hello@example.com')->send(new NewContact($new_lead));
+
+        return response()->json([
+            'success' => true
+        ]);
+    }
 }
