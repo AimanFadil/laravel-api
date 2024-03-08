16a631554b6e0e159da75a3d1b6f32bc85a604ab
AimanFadil <aimanbello2001@gmail.com>
2024-03-08 14:38:46 +0100

    migration and contact mail done

---
+++ resources/views/emails/contact-email.blade.php
@@ -0,0 +1,10 @@
+<h1>MAIL PER L'ADMIN</h1>
+<p>
+    Nome : {{ $lead->nome }} <br>
+    Cognome : {{ $lead->cognome }} <br>
+    Numero : {{ $lead->cell }} <br>
+    Mail : {{ $lead->email }} <br>
+    Testo: <br>
+    {{ $lead->messaggio }}
+
+</p>
